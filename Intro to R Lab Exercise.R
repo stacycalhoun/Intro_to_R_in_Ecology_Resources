7ecc5da5bf1ebc28258bf3d88cf37bcbbe3547e8
Stacy <stacy.calhoun1@louisiana.edugit config --global user.email stacy.calhoun1@louisiana.edugit config --global user.email>
2020-04-21 14:18:57 -0500

    Description of Intro to R script

---
+++ Intro to R Lab Exercise.R
@@ -0,0 +1,6 @@
+#Introduction to Coding in R lab exercise. In this script, you will learn some of the basics 
+#of coding in R. Additional resources can be found in the GitHub repository: https://github.com/stacycalhoun/R_resources
+#Compiled by Stacy Calhoun
+
+
+
